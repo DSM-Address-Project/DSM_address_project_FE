67d1feb393569c2abeb57b98440243a461a39227
dutexion <dutexion@gmail.com>
2023-12-01 15:49:56 +0900

    fix :: merge conflict

--- packages/dsm_address_project/src/App.tsx
+++ packages/dsm_address_project/src/App.tsx
@@ -1,9 +1,23 @@
-import ResultBox from './components/ResultBox'
+import { useState } from "react";
+import TextFiled from "./components/TextFiled";
+import TipContainer from "./components/TipContainer";
 
 function App() {
+  const [keyword, setKeyword] = useState(""); //검색결과 state
+  console.log(keyword);
+
   return (
-    <ResultBox/>
-  )
+    <>
+      <div>
+        <TextFiled
+          onSubmit={(keyword) => {
+            setKeyword(keyword);
+          }}
+        />
+      </div>
+      <TipContainer />
+    </>
+  );
 }
 
-export default App
+export default App;
